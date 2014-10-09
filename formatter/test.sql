0de27bb3535d417b752034190d293051b7f7ee74
Brian Ballsun-Stanton <brian@fedarch.org>
2014-10-09 16:21:44 +1100

    ready to query

--- formatter/test.sql
+++ formatter/test.sql
@@ -1,5 +1,10 @@
-select 1;
-
--- select 1;
-
-select 1;
+  select uuid, aenttypeid, aenttypename, group_concat(coalsece(formatstring, vocabname, measure, freetext, certainty), appendcharacterstring) as response, null as deleted
+  from latestNonDeletedArchent 
+  JOIN aenttype using (aenttypeid)
+  JOIN idealaent using (aenttypeid)
+  join attributekey using (attributeid)
+  join latestNonDeletedAentValue using (uuid, attributeid)  
+  left outer join vocabulary using (attributeid, vocabid)
+ WHERE isIdentifier = 'true'
+ group by uuid, attributeid
+ order by uuid, aentcountorder;
--- formatter/testFormat.sh
+++ formatter/testFormat.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+echo $1
+
 rvm use 2.1.1@faims
+
 $(rvm which ruby) string_formatter_tester.rb db.sqlite3 $1
