ae2e382c30c47b594699d9fc2583d01a159c28d8
Brian Ballsun-Stanton <brian@fedarch.org>
2015-05-15 12:31:02 +1000

    debug

--- data_schema_generator.py
+++ data_schema_generator.py
@@ -40,63 +40,12 @@ def getRowValue(row, format, column_name):
 
 #Implementation of perl's autovivification feature.
 class AutoVivification(dict):
-    """Implementation of perl's autovivification feature. Has features from both dicts and lists,
-    dynamically generates new subitems as needed, and allows for working (somewhat) as a basic type.
-    """
     def __getitem__(self, item):
-        if isinstance(item, slice):
-            d = AutoVivification()
-            items = sorted(self.iteritems(), reverse=True)
-            k,v = items.pop(0)
-            while 1:
-                if (item.start < k < item.stop):
-                    d[k] = v
-                elif k > item.stop:
-                    break
-                if item.step:
-                    for x in range(item.step):
-                        k,v = items.pop(0)
-                else:
-                    k,v = items.pop(0)
-            return d
         try:
             return dict.__getitem__(self, item)
         except KeyError:
             value = self[item] = type(self)()
             return value
-
-    def __add__(self, other):
-        """If attempting addition, use our length as the 'value'."""
-        return len(self) + other
-
-    def __radd__(self, other):
-        """If the other type does not support addition with us, this addition method will be tried."""
-        return len(self) + other
-
-    def append(self, item):
-        """Add the item to the dict, giving it a higher integer key than any currently in use."""
-        if self.count(item) == 0:
-            self.__setitem__(0, item)
-        largestKey = sorted(self.keys())[-1]
-        if isinstance(largestKey, str):
-            self.__setitem__(0, item)
-        elif isinstance(largestKey, int):
-            self.__setitem__(largestKey+1, item)
-
-    def count(self, item):
-        """Count the number of keys with the specified item."""
-        return sum([1 for x in self.items() if x == item])
-
-    def __eq__(self, other):
-        """od.__eq__(y) <==> od==y. Comparison to another AV is order-sensitive
-        while comparison to a regular mapping is order-insensitive. """
-        if isinstance(other, AutoVivification):
-            return len(self)==len(other) and self.items() == other.items()
-        return dict.__eq__(self, other)
-
-    def __ne__(self, other):
-        """od.__ne__(y) <==> od!=y"""
-        return not self == other
 
 class Term:
     def __init__(self, term, description, parent, index, pictureurl, arch16n):
@@ -148,6 +97,7 @@ vocabTable = AutoVivification()
 for entry in html['feed']['entry']:
     row = entry['content']['$t'].encode('utf-8').strip()
     entity = getRowValue(row, format, 'entityname')
+    print entity
     if getRowValue(row, format, 'element') != "":
         vocabTable[entity]['enttype'] = getRowValue(row, format, 'element')
         vocabTable[entity]['description'] = getRowValue(row, format, 'description')
@@ -166,17 +116,12 @@ for entry in html['feed']['entry']:
                 found = True
         if found:
             t = Term(getRowValue(row, format, 'term'), getRowValue(row, format, 'description'), getRowValue(row, format, 'parent'), len(p['terms']), getRowValue(row, format, 'pictureurl'), getRowValue(row, format, 'arch16n'))
-            print "%s %s %s %s" % (attr['attribute'], t.term, t.index, t.parent)
             
-
-            if int(t.parent) != -1:                                
-                p['terms'][int(t.parent)].children.append(int(t.index))
-
+            if int(t.parent) != -1:
+                p['terms'][int(t.parent)].children.append(t.index)
             else:
                 p['parents'].append(t)
             p['terms'].append(t)
-
-
         else:
             p = AutoVivification()
             p['attribute'] = getRowValue(row, format, 'attribute')
@@ -187,6 +132,7 @@ for entry in html['feed']['entry']:
             if p['type'] == "enum" or p['type'] == "hierarchical":
                 p['terms'] = []
                 p['parents'] = []
+            print "%s %s %s" % (entity, attribute, type(vocabTable[entity]['properties']))
             vocabTable[entity]['properties'].append(p)
 
 print """<?xml version="1.0" encoding="UTF-8"?>
