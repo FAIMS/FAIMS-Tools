e866a3662451ce374114a7bceced270ab26a3a7b
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2015-09-02 14:56:44 +1000

    Generated files now use underscores instead of dashes in their name

--- generators/christian/generate.sh
+++ generators/christian/generate.sh
@@ -8,10 +8,10 @@ mkdir -p module
 mkdir -p wireframe
 
 $proc1 generator/arch16n.xsl     module.xml | sort | uniq >module/english.0.properties
-$proc1 generator/data-schema.xsl module.xml               >module/data-schema.xml
-$proc1 generator/ui-logic.xsl    module.xml               >module/ui-logic.bsh
-$proc1 generator/ui-schema.xsl   module.xml               >module/ui-schema.xml
-$proc1 generator/ui-styling.xsl  module.xml               >module/ui-styling.css
+$proc1 generator/data-schema.xsl module.xml               >module/data_schema.xml
+$proc1 generator/ui-logic.xsl    module.xml               >module/ui_logic.bsh
+$proc1 generator/ui-schema.xsl   module.xml               >module/ui_schema.xml
+$proc1 generator/ui-styling.xsl  module.xml               >module/ui_styling.css
 $proc1 generator/validation.xsl  module.xml               >module/validation.xml
 
 gawk    -f generator/arch16nForWireframe.awk   module/english.0.properties >wireframe/arch16n.xml
