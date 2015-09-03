62a7dd0eef74e4271e804339fc75ea672315f764
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2015-09-03 17:12:04 +1000

    Generalised the features a little

--- xml-tools/cp-attrib-location/main.py
+++ xml-tools/cp-attrib-location/main.py
@@ -1,36 +1,44 @@
 # Copy the attributeLocation from draft3.csv to attributes.csv.  Output to
 # stdout.
-
+# TODO specify which column to join on
 import sys
+
+if len(sys.argv) < 3:
+    sys.stderr.write('SYNOPSIS\n')
+    sys.stderr.write('       python %s SOURCE TARGET\n' % sys.argv[0])
+    exit()
+
+source = sys.argv[1]
+target = sys.argv[2]
 
 id2EntName = {}
 
-with open('draft3.csv') as f:
-    linesD = f.read().splitlines()
-with open('attributes.csv') as f:
-    linesA = f.read().splitlines()
+with open(source) as f:
+    linesS = f.read().splitlines()
+with open(target) as f:
+    linesT = f.read().splitlines()
 
-for i in range(len(linesD)):
-    linesD[i] = linesD[i].split(',')
-for i in range(len(linesA)):
-    linesA[i] = linesA[i].split(',')
+for i in range(len(linesS)):
+    linesS[i] = linesS[i].split(',')
+for i in range(len(linesT)):
+    linesT[i] = linesT[i].split(',')
 
-for i in range(len(linesD)):
-    idD   = linesD[i][0]
-    nameD = linesD[i][2]
-    id2EntName[idD] = nameD
+for i in range(len(linesS)):
+    idS   = linesS[i][0]
+    nameS = linesS[i][2]
+    id2EntName[idS] = nameS
 
-linesA[0].insert(2, 'attributeLocation')
-for i in range(1, len(linesA)):
-    idA = linesA[i][0]
-    if idA in id2EntName:
-        nameD = id2EntName[idA]
-        linesA[i].insert(2, nameD)
+linesT[0].insert(2, 'attributeLocation')
+for i in range(1, len(linesT)):
+    idT = linesT[i][0]
+    if idT in id2EntName:
+        nameS = id2EntName[idT]
+        linesT[i].insert(2, nameS)
     else:
-        linesA[i].insert(2, '')
+        linesT[i].insert(2, '')
 
-for i in range(len(linesA)):
-    linesA[i] = ','.join(linesA[i])
+for i in range(len(linesT)):
+    linesT[i] = ','.join(linesT[i])
 
-for i in range(len(linesA)):
-    sys.stdout.write(linesA[i] + '\n')
+for i in range(len(linesT)):
+    sys.stdout.write(linesT[i] + '\n')
