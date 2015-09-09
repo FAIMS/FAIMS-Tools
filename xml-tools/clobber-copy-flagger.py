fe42ecb5ed4f52dce1b6431e9df0b2c8eabab054
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2015-09-09 20:48:45 +1000

    Added feature to allow flagging by XPath expression

--- xml-tools/clobber-copy-flagger.py
+++ xml-tools/clobber-copy-flagger.py
@@ -33,19 +33,28 @@ reservedCopy = '__RESERVED_CP__'
 
 if len(sys.argv) < 2:
     sys.stderr.write('SYNOPSIS\n')
-    sys.stderr.write('       python %s XML [FLAGS]\n' % sys.argv[0])
+    sys.stderr.write('       python %s XML\n'          % sys.argv[0])
+    sys.stderr.write('       python %s XML -f FLAGS\n' % sys.argv[0])
+    sys.stderr.write('       python %s XML -x XPATH\n' % sys.argv[0])
     sys.stderr.write('\n')
     sys.stderr.write('DESCRIPTION\n')
     sys.stderr.write('   Adds the attribute __RESERVED__CP__="true" to each node of XML specified\n')
-    sys.stderr.write('   in FLAGS. If FLAGS is not given, all leaf nodes are marked.\n')
+    sys.stderr.write('   in FLAGS or XPATH. If FLAGS is not given, all leaf nodes are marked.\n')
     sys.stderr.write('\n')
-    sys.stderr.write('   Nodes in the FLAGS file are specified in the format:\n')
-    sys.stderr.write('       tabgroup/tab/xpath-expression\n')
-    sys.stderr.write('   For example, writing\n')
-    sys.stderr.write('       Diary/AreaCode/lookup\n')
-    sys.stderr.write('   is equivalent to the following XPath expression:\n')
-    sys.stderr.write('           dataSchema/ArchaeologicalElement[@name="Diary"]/property[@name="AreaC\n')
-    sys.stderr.write('ode]/lookup\n')
+    sys.stderr.write('OPTIONS\n')
+    sys.stderr.write('   -f FLAGS\n')
+    sys.stderr.write('       Flag the nodes in XML according to the paths in the file given by FLAGS.\n')
+    sys.stderr.write('\n')
+    sys.stderr.write('       Nodes in the FLAGS file are specified in the format:\n')
+    sys.stderr.write('           tabgroup/tab/xpath-expression\n')
+    sys.stderr.write('       For example, writing\n')
+    sys.stderr.write('           Diary/AreaCode/lookup\n')
+    sys.stderr.write('       is equivalent to the following XPath expression:\n')
+    sys.stderr.write('               dataSchema/ArchaeologicalElement[@name="Diary"]/property[@name="A\n')
+    sys.stderr.write('reaCode]/lookup\n')
+    sys.stderr.write('\n')
+    sys.stderr.write('   -x XPATH\n')
+    sys.stderr.write('       Flag the nodes in XML according to the XPath expression in XPATH.')
     exit()
 
 # Parse XML
@@ -54,11 +63,15 @@ tree = etree.parse(sys.argv[1], parser)
 tree = tree.getroot()
 
 # Generate or parse XPath expressions indicating which nodes should be marked
+flagPaths = []
 if len(sys.argv) == 2:
-    flagPaths = []
     flagPaths.append('//*[not(*)]') # Leaf nodes
-elif len(sys.argv) == 3:
-    flagPaths = parseFlagsFromFile(sys.argv[2])
+elif len(sys.argv) == 4 and sys.argv[2] == '-f':
+    flagPaths = parseFlagsFromFile(sys.argv[3])
+elif len(sys.argv) == 4 and sys.argv[2] == '-x':
+    flagPaths.append(sys.argv[3])
+else:
+    exit()
 
 # Mark nodes
 for xp in flagPaths:
