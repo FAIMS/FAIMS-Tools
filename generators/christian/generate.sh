dfdc1f0a8484d130c35dfdc770ea791e0bede878
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2015-12-16 15:52:57 +1100

    Need to cd into right dir before running @POSTPROC command

--- generators/christian/generate.sh
+++ generators/christian/generate.sh
@@ -13,6 +13,7 @@ then
 else
     module=$1
 fi
+modulePath=$( dirname "$module" )
 
 $proc1 generator/arch16n.xsl     $module | sort | uniq >module/english.0.properties
 $proc1 generator/data-schema.xsl $module               >module/data_schema.xml
@@ -25,5 +26,6 @@ gawk    -f generator/arch16nForWireframe.awk   module/english.0.properties >wire
 $proc2 -xsl:generator/wireframeElements.xsl  -s:module/ui_schema.xml        >wireframe/wireframeElements.sh
 
 # Handle post-processing directive
+cd "$modulePath"
 cmd=$( grep "@POSTPROC:" "$module" | head -n 1 | sed -rn 's/^\s*<!--\s*@POSTPROC:\s*(.*[^ ])\s*-->\s*$/\1/p' )
 eval $cmd # Yes, I've heard of the "eval is evil" dogma
