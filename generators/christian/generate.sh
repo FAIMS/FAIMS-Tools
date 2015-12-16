8c91e85b0b72a70e5fc9f898763fb666a5c774b7
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2015-12-16 15:40:21 +1100

    "Words are hard"

--- generators/christian/generate.sh
+++ generators/christian/generate.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 proc1="xsltproc"    # Lord, forgive me
@@ -23,3 +23,7 @@ $proc1 generator/validation.xsl  $module               >module/validation.xml
 
 gawk    -f generator/arch16nForWireframe.awk   module/english.0.properties >wireframe/arch16n.xml
 $proc2 -xsl:generator/wireframeElements.xsl  -s:module/ui_schema.xml        >wireframe/wireframeElements.sh
+
+# Handle post-processing directive
+cmd=$( grep "@POSTPROC:" "$module" | head -n 1 | sed -rn 's/^\s*<!--\s*@POSTPROC:\s*(.*[^ ])\s*-->\s*$/\1/p' )
+eval $cmd # Yes, I've heard of the "eval is evil" dogma
