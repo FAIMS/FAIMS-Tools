ac64d4c60d336f6e2061d882619003b11fac650e
Brian Ballsun-Stanton <brian@fedarch.org>
2016-03-23 15:36:37 +1100

    fixed multiuser tmp

--- gv/makeElement.sh
+++ gv/makeElement.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-tmp=$(mktemp)
+tmp=$(mktemp -d)
 
 
 #Takes as arguments: Name, Label, type, numColumns, HasAnnotation, HasCertainty, HasInfo, Required, ReadOnly, Path
