FAIMS/FAIMS-Tools

commit b875e19bd5a545c6f9d0238588402840831b45d4

<gps/> no clears its fields upon duplication

generators/christian/generator/module/ui-logic.xsl
@@ -1376,6 +1376,25 @@ fillInGPS(String tabgroup) {
     }
   });
 }
+
+clearGpsInTabGroup(String tabgroup) {
+</xsl:text>
+    <xsl:call-template name="take-from-gps-mappings"/>
+<xsl:text>
+
+  String tabRef = tabgroupToTabRef.get(tabgroup);
+  if (isNull(tabRef)) return;
+
+  clearGpsInTab(tabRef);
+}
+
+clearGpsInTab(String tabRef) {
+  setFieldValue(tabRef + "Accuracy"  , "");
+  setFieldValue(tabRef + "Latitude"  , "");
+  setFieldValue(tabRef + "Longitude" , "");
+  setFieldValue(tabRef + "Easting"   , "");
+  setFieldValue(tabRef + "Northing"  , "");
+}
 </xsl:text>
     </xsl:if>
 
@@ -1812,6 +1831,9 @@ bindOnEvents();
     <xsl:value-of select="$newline"/>
     <xsl:value-of select="$newline"/>
     <xsl:call-template name="tabgroup-duplicate-incautonum"/>
+    <xsl:value-of select="$newline"/>
+    <xsl:text>  clearGpsInTabGroup(tabgroup);</xsl:text>
+    <xsl:value-of select="$newline"/>
     <xsl:value-of select="$newline"/>
     <xsl:call-template name="tabgroup-duplicate-exclusions-1" />
 <xsl:text>

commit d9fe847106d0eca091156de363bf0408160587f6

Fixed bug introduced by b875e19bd5a545c6f9d0238588402840831b45d4

generators/christian/generator/module/ui-logic.xsl
@@ -1302,12 +1302,12 @@ search(){
     </xsl:if>
 
     <!-- Take From GPS Button -->
-    <xsl:if test="/module/*/*/gps">
       <xsl:text>
 /******************************************************************************/
 /*                          TAKE FROM GPS BUTTON(S)                           */
 /******************************************************************************/
 </xsl:text>
+    <xsl:if test="/module/*/*/gps">
 <xsl:call-template name="take-from-gps-bindings"/>
 <xsl:text>
 /* Takes the current point using gps. */
@@ -1376,7 +1376,9 @@ fillInGPS(String tabgroup) {
     }
   });
 }
-
+</xsl:text>
+    </xsl:if>
+<xsl:text>
 clearGpsInTabGroup(String tabgroup) {
 </xsl:text>
     <xsl:call-template name="take-from-gps-mappings"/>
@@ -1396,7 +1398,6 @@ clearGpsInTab(String tabRef) {
   setFieldValue(tabRef + "Northing"  , "");
 }
 </xsl:text>
-    </xsl:if>
 
     <xsl:if test="//*[contains(@f, 'autonum')] and not(//autonum)">
       <xsl:text>// ERROR: field(s) flagged with 'autonum' but no autonum tag exists</xsl:text>