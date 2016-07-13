7d412a90b0de00f91481ab8085591e6a8c2d7c09
DoxasticFox <doxasticfox@github.com>
2016-07-13 17:45:36 +1000

    Some more changes for Matt

--- generators/christian/generator/module/uischema.py
+++ generators/christian/generator/module/uischema.py
@@ -107,12 +107,12 @@ util.schema.annotateWithTypes(tree)
 ################################################################################
 uiSchema = getUiSchema(tree)
 
-print etree.tostring(
-        tree,
-        pretty_print=True,
-        xml_declaration=True,
-        encoding='utf-8'
-)
+#print etree.tostring(
+        #tree,
+        #pretty_print=True,
+        #xml_declaration=True,
+        #encoding='utf-8'
+#)
 
 print etree.tostring(
         uiSchema,
