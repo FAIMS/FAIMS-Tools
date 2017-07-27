816721a0a6a582bfb45554e7bf2eb4a74859c468
DoxasticFox <doxasticfox@github.com>
2017-07-27 15:15:21 +1000

    AUTOGEN-4

--- generators/christian/util/schema.py
+++ generators/christian/util/schema.py
@@ -501,10 +501,8 @@ def getSignupGuide(node):
     textMd += ' 1 lowercase letter, 1 digit and 1 symbol. Examples of strong '
     textMd += 'passwords:\n'
     textMd += '\n'
-    textMd += '* 2.Howareyou\n'
-    textMd += '* Artifact5.\n'
-    textMd += '* Banana@453\n'
-    textMd += '* Binford_1980\n'
+    textMd += '* Bond@007\n'
+    textMd += '* Dig.123\n'
 
     g = Element   (   tagGuide, attribsGuide)
     m = SubElement(g, tagMd                 ); m.text = textMd
