7587b7fa8401b97975d54549d8dfc6def230d592
DoxasticFox <doxasticfox@github.com>
2017-09-04 20:47:34 +1000

    Added installer and documentation for requirements

---
+++ generators/christian/install-dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Tested on: 16.04.03 LTS
+
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please run me with root privileges. Example:"
+    echo "    sudo $0"
+    exit
+fi
+
+add-apt-repository universe
+apt-get update
+apt-get install \
+        bsh \
+        default-jre \
+        gawk \
+        git \
+        graphviz \
+        libsaxonb-java \
+        pandoc \
+        python-cairosvg \
+        python-lxml \
+        -y
