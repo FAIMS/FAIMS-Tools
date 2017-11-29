e8e2ff2124c431b83fae0c8be051950d483afb53
Brian Ballsun-Stanton <brian@fedarch.org>
2017-11-29 16:25:25 +1100

    improved formatter

--- formatter/string_formatter_tester.rb
+++ formatter/string_formatter_tester.rb
@@ -2,13 +2,43 @@
 
 require 'sqlite3'
 require 'antlr3'
-require 'benchmark'
-
+require 'table_print'
 require_relative 'string_formatter'
+require 'arrayfields'
 
 def spatialite_library
   return 'libspatialite.dylib' if (/darwin/ =~ RUBY_PLATFORM) != nil
-  return 'libspatialite.so'
+  if `. /etc/lsb-release 2>/dev/null ; echo $DISTRIB_CODENAME`.strip == 'trusty'
+    return 'libspatialite.so'
+  else
+    return 'mod_spatialite.so'
+  end
+
+
+end
+
+
+
+class Hash
+  # Returns a hash that includes everything but the given keys.
+  #   hash = { a: true, b: false, c: nil}
+  #   hash.except(:c) # => { a: true, b: false}
+  #   hash # => { a: true, b: false, c: nil}
+  #
+  # This is useful for limiting a set of parameters to everything but a few known toggles:
+  #   @person.update(params[:person].except(:admin))
+  def except(*keys)
+    dup.except!(*keys)
+  end
+
+  # Replaces the hash without the given keys.
+  #   hash = { a: true, b: false, c: nil}
+  #   hash.except!(:c) # => { a: true, b: false}
+  #   hash # => { a: true, b: false }
+  def except!(*keys)
+    keys.each { |key| delete(key) }
+    self
+  end
 end
 
 db_file = ARGV.shift
@@ -17,6 +47,7 @@ sql_file = ARGV.shift
 db = SQLite3::Database.new(db_file)
 db.enable_load_extension(true)
 db.execute("select load_extension('#{spatialite_library}')")
+db.results_as_hash = true
 
 db.create_function('debug_format', -1) do |func, *args|
   $debug = true
@@ -44,9 +75,20 @@ File.open(sql_file, 'r') do |file|
       sql = sql.gsub(/(\n|\s|\t)+/, ' ').strip
       unless sql.nil? or sql == ''
         result = db.execute(sql) 
+        
+        resultHash = []
         result.each do |r|
-          puts r.join("\t")
+        #  puts r.join("\t")
+          #puts r
+          for i in 0..r.length/2-1
+            r.delete(i)
+          end
+
+          
+          resultHash << r
         end
+        puts sql
+        tp resultHash
         puts ""
       end
       sql = ""
@@ -60,6 +102,6 @@ File.open(sql_file, 'r') do |file|
     end
   end
   
-  puts sql
+  
 end
 
