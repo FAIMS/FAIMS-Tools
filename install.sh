1ce316887137fc8723c2517500bf07a819f6e92d
DoxasticFox <doxasticfox@github.com>
2018-06-13 20:01:01 +1000

    Removed currently unused files

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install bc wget xsltproc poppler-utils libdevil-dev libgs-dev librsvg2-bin librsvg2-dev gcj-4.8-jre-headless libpangocairo-1.0-0 libcairo1.10-cil libcairo2 libcairo2-dbg expat lib64expat1 libfreetype6 libfreetype6-dev libgd-dev fontconfig-config libfontconfig1 libfontconfig1-dev libpng++-dev libpng12-0 libpng12-dev libpango-1.0-0 libpango1.0-dev zlib1g zlibc libcairo2-dev libsaxonb-java -y && sudo apt-get build-dep graphviz
-#cd /tmp
-#wget http://www.graphviz.org/pub/graphviz/CURRENT/graphviz-working.tar.gz
-#tar -xzf graphviz-working.tar.gz
-#cd `find . -name "graphviz*" -type d`
